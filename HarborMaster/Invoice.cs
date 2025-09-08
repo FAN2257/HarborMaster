4470920a9197392b384bb99789ed86808fef8c0d
HaidarFaruqi <haidarfaruqi126@gmail.com>
2025-09-08 20:47:16 +0700

    feat: Add Invoice and Notification Class Model

---
+++ HarborMaster/Invoice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarborMaster
+{
+    internal class Invoice
+    {
+        public int invoiceID { get; set; }
+        public string shipID { get; set; }
+        public decimal totalAmount { get; set; }
+        public string paymentStatus { get; set; }
+        public DateTime issuedDate { get; set; }
+
+        public void generateInvoice(string shipID, decimal amount)
+        {
+            this.shipID = shipID;
+            this.totalAmount = amount;
+            this.paymentStatus = "Unpaid";
+            this.issuedDate = DateTime.Now;
+            // Logic to save invoice to database or send to accounting system
+        }
+
+        public PortService[] addService(PortService[] services)
+        {
+            // Logic to add services to the invoice
+            return services;
+        }
+
+        public decimal calculateTotal()
+        {
+            return totalAmount;
+        }
+
+        public void markAsPaid()
+        {
+            this.paymentStatus = "Paid";
+            // Logic to update invoice status in database
+        }
+
+    }
+}
---
+++ HarborMaster/Notification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarborMaster
+{
+    internal class Notification
+    {
+        public int notificationID { get; set; }
+        public string message { get; set; }
+        public string type { get; set; }
+        public DateTime timestamp { get; set; }
+
+        public void SendNotification(string message, string type)
+        {
+            this.message = message;
+            this.type = type;
+            this.timestamp = DateTime.Now;
+            // Logic to send notification (e.g., email, SMS, in-app)
+        }
+
+        public void markAsRead()
+        {
+            // Logic to mark notification as read
+        }
+    }
+}
